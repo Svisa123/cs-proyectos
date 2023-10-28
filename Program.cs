2a7b48857313e59478fc6825a07c8be787a0973d
unknown <matteomeojunior@gmail.com>
2023-10-28 17:13:03 -0400

    chat

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chat App - Ingrese su nombre:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"Bienvenido, {username}!");
+            Console.WriteLine("Escriba 'exit' para salir.");
+
+            string message;
+
+
+            do
+            {
+                message = Console.ReadLine();
+                Console.WriteLine($"{username}: {message}");
+            } while (message != "exit");
+
+            // while ((message = Console.ReadLine()) != "exit")
+            // {
+            //     Console.WriteLine($"{username}: {message}");
+            // }
+        }
+    }
+}
