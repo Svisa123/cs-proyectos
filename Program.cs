535960db479c5c7a4636d401ae9f2a75eadbdfbf
unknown <matteomeojunior@gmail.com>
2023-10-29 16:00:37 -0400

    init

--- Program.cs
+++ Program.cs
@@ -6,25 +6,7 @@ namespace ChatApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Chat App - Ingrese su nombre:");
-            string username = Console.ReadLine();
-
-            Console.WriteLine($"Bienvenido, {username}!");
-            Console.WriteLine("Escriba 'exit' para salir.");
-
-            string message;
-
-
-            do
-            {
-                message = Console.ReadLine();
-                Console.WriteLine($"{username}: {message}");
-            } while (message != "exit");
-
-            // while ((message = Console.ReadLine()) != "exit")
-            // {
-            //     Console.WriteLine($"{username}: {message}");
-            // }
+           
         }
     }
 }
